eef1a80b3a2006eb8bfc18f5e64404839e6f31e2
Toby Bettridge <toby@heavypenguin.com>
2020-12-01 19:00:17 +0000

    All coding standards fixed.

--- js/utils.js
+++ js/utils.js
@@ -2,32 +2,34 @@
  * File utils.js.
  */
 ( function() {
-  window.onscroll = function() {scrollMenu()};
+	window.onscroll = function() {
+		scrollMenu();
+	};
 
-  function scrollMenu() {
-    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-      document.getElementById("site-navigation").className = "main-navigation scrolled";
-    } else {
-      document.getElementById("site-navigation").className = "main-navigation";
-    }
-  }
+	function scrollMenu() {
+		if ( document.body.scrollTop > 50 || document.documentElement.scrollTop > 50 ) {
+			document.getElementById( 'site-navigation' ).className = 'main-navigation scrolled';
+		} else {
+			document.getElementById( 'site-navigation' ).className = 'main-navigation';
+		}
+	}
 
-  var acc = document.getElementsByClassName("accordion");
-  var i;
+	const acc = document.getElementsByClassName( 'accordion' );
+	let i;
 
-  for (i = 0; i < acc.length; i++) {
-    acc[i].addEventListener("click", function() {
-      /* Toggle between adding and removing the "active" class,
-      to highlight the button that controls the panel */
-      this.classList.toggle("active");
+	for ( i = 0; i < acc.length; i++ ) {
+		acc[ i ].addEventListener( 'click', function() {
+			/* Toggle between adding and removing the 'active' class,
+			to highlight the button that controls the panel */
+			this.classList.toggle( 'active' );
 
-      /* Toggle between hiding and showing the active panel */
-      var panel = this.nextElementSibling;
-      if (panel.style.display === "block") {
-        panel.style.display = "none";
-      } else {
-        panel.style.display = "block";
-      }
-    });
-  }
+			/* Toggle between hiding and showing the active panel */
+			const panel = this.nextElementSibling;
+			if ( panel.style.display === 'block' ) {
+				panel.style.display = 'none';
+			} else {
+				panel.style.display = 'block';
+			}
+		} );
+	}
 }() );
